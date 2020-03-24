59d919b23366e6d68cd113bc0d4371ae5f292b83
George Chen <georgechen1338@gmail.com>
2020-03-24 23:01:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,8 +13,8 @@ end
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+    elsif num = 2
+       "HAM HAM HAM IN MY TUMMY"
   end
 end
 
